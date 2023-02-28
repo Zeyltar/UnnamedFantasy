d26421452193775354bc55a80a45a24033e509ec
Valbatoze <edwin987@hotmail.fr>
2023-02-28 23:47:08 +0100

    add level

---
+++ Assets/MapTMX/inca_back2.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="inca_back2" tilewidth="32" tileheight="32" tilecount="230" columns="5">
+ <image source="../Sprites/Tiles/inca_back2.png" width="160" height="1472"/>
+ <tile id="0" probability="0.01"/>
+ <tile id="1" probability="0.01"/>
+ <tile id="2" probability="0.01"/>
+ <tile id="3" probability="0.01"/>
+ <tile id="5" probability="0.01"/>
+ <tile id="6" probability="0.01"/>
+ <tile id="7" probability="0.01"/>
+ <tile id="8" probability="0.01"/>
+ <tile id="9" probability="0.01"/>
+ <tile id="220" probability="0.01"/>
+</tileset>
