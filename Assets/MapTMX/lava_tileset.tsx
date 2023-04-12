fa58a6d6f48834d7b1824f9898a160011b11886f
Zeyltar <florian.poinsot@ynov.com>
2023-04-12 17:00:22 +0200

    add lava obstacle

---
+++ Assets/MapTMX/lava_tileset.tsx
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="lava_tileset" tilewidth="32" tileheight="32" tilecount="4" columns="4">
+ <image source="../Sprites/lava/Lava.png" width="128" height="32"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="4" x="0" y="4" width="32" height="28"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="200"/>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="2" duration="200"/>
+   <frame tileid="3" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
